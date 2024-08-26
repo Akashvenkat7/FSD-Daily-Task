f483905c74dd46f689b8e9d473d6dd3b31934432
akaz200 <akashvenkat4454@gmail.com>
2024-08-26 11:00:09 +0530

    first commit

--- Day 3/script.js
+++ Day 3/script.js
@@ -92,95 +92,95 @@ result();
 
 // //if Else statements  
 
-let login = true
+// let login = true
 
-if (login == true) {
-     console.log("login succesfully");
+// if (login == true) {
+//      console.log("login succesfully");
      
-} else {
-     console.log("invalid login details");
+// } else {
+//      console.log("invalid login details");
      
-}
+// }
 
-//else if
+// //else if
 
-let proPrice = 884;
+// let proPrice = 884;
 
-if (proPrice >= 1000) {
-     console.log(">=1000");
+// if (proPrice >= 1000) {
+//      console.log(">=1000");
      
-} else if(proPrice >= 900){
-     console.log(">=900");
+// } else if(proPrice >= 900){
+//      console.log(">=900");
      
-}else if(proPrice >=800){
-     console.log(">800");
+// }else if(proPrice >=800){
+//      console.log(">800");
      
-}else{
-     console.log("below 800");
+// }else{
+//      console.log("below 800");
      
-}
+// }
 
-// nested if
+// // nested if
 
-let prodPrice = 468;
-if(prodPrice >=800){
-     if(prodPrice <=1000){
-          console.log("<=1000");
+// let prodPrice = 468;
+// if(prodPrice >=800){
+//      if(prodPrice <=1000){
+//           console.log("<=1000");
           
-     }else{
-          console.log(">1000");
+//      }else{
+//           console.log(">1000");
           
-     }
-}else{
-     console.log("<800");
+//      }
+// }else{
+//      console.log("<800");
      
-}
+// }
 
-///ternary operator
+// ///ternary operator
 
-let price = 77
-const resl = price > 50 ? "greater than 50" : "Lesser than 50"
-console.log(resl);
+// let price = 77
+// const resl = price > 50 ? "greater than 50" : "Lesser than 50"
+// console.log(resl);
 
-/// Switch case
+// /// Switch case
 
-let order = 2
-let msg;
+// let order = 2
+// let msg;
 
-switch(order){
-     case 1:
-          msg='I Ordered Ipad';
-          break;
-     case 2:
-          msg='I Ordered Macbook';
-          break;
-     case 3:
-          msg='I Ordered Iphone 16 Pro Max';
-          break;
-     default:
-          msg='Invalid';          
-}
-console.log(msg);
+// switch(order){
+//      case 1:
+//           msg='I Ordered Ipad';
+//           break;
+//      case 2:
+//           msg='I Ordered Macbook';
+//           break;
+//      case 3:
+//           msg='I Ordered Iphone 16 Pro Max';
+//           break;
+//      default:
+//           msg='Invalid';          
+// }
+// console.log(msg);
 
-//while //entry check
+// //while //entry check
 
-let i=3
-while (i<=5) {
-     console.log("i=",i);
-     i++;
+// let i=3
+// while (i<=5) {
+//      console.log("i=",i);
+//      i++;
      
-}
+// }
 
-//do while //exit check
-let j=3
-do{
-    console.log("i=",i);
-    i--;
-}while(j>=0);
+// //do while //exit check
+// let j=3
+// do{
+//     console.log("i=",i);
+//     i--;
+// }while(j>=0);
 
-//for loop
+// //for loop
 
-for(let i=0;i<5;i++){
-     console.log("i",i);
+// for(let i=0;i<5;i++){
+//      console.log("i",i);
      
-}
+// }
